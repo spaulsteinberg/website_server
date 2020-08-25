bb9e6ebe776e2699c2eb442d9ed3314d49305826
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-08-25 13:11:05 -0500

    close #3 done

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql');
 const Validator = require('validatorjs');
-const connectParams = require('./important');
+const connectParams = require('../important');
+const nodemailer = require('nodemailer');
 
 //choose a port to run server
 const PORT = 3000;
@@ -60,6 +61,7 @@ app.post('/setcontact', function(request, response){
             }
             else {
                 response.send({"database_success": `Your feedback has been received, ${request.body.firstName}!`});
+                sendAlertEmail(request.body);
             }
         });
     }
@@ -72,5 +74,38 @@ app.post('/setcontact', function(request, response){
 app.listen(PORT, function(){
     console.log("Listening on port...", PORT);
 });
+var transport = nodemailer.createTransport({
+    host: connectParams.email.HOST,
+    service: connectParams.email.SERVICE,
+    auth: {
+        type: 'OAuth2',
+        user: connectParams.email.AUTH.USER,
+        clientId: connectParams.email.AUTH.CLIENT_ID,
+        clientSecret: connectParams.email.AUTH.SECRET_ID,
+        refreshToken: connectParams.email.AUTH.REFRESH_TOKEN,
+        accessToken: connectParams.email.AUTH.ACCESS_TOKEN
+    }
+});
+var mailParams = {
+    from: connectParams.email.OPTIONS.FROM,
+    to: connectParams.email.OPTIONS.TO,
+}
+function getFormattedDate(){
+    let oldDate = new Date();
+    return `${oldDate.toString()}`
+}
+function sendAlertEmail(content){
+    mailParams.subject = `WEBSITE FEEDBACK FROM ${getFormattedDate()}`;
+    mailParams.html = `<p>First Name: ${content.firstName}</p>
+                       <p>Last Name: ${content.lastName}</p>
+                       <p>Email: ${content.email}</p>
+                       <p>Phone: ${content.phone}</p>
+                       <p>Description: ${content.description}</p>`;
+    transport.sendMail(mailParams, function(error, info){
+        if (error) console.log(error);
+        else console.log(info.response, Date.now());
+        //should probably write this to a log
+    })
+}
 
 /* RUN ON NODE WITH: $ node server */

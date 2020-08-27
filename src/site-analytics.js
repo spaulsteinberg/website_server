355fd76fa33dbd0ab0fde6d738ced1c2542cee92
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-08-26 21:58:57 -0500

    created new analytic file to fetch data for website

---
+++ src/site-analytics.js
@@ -0,0 +1,5 @@
+//file for analytics
+
+//google analytic api -- use map locations and overall data
+// grab the new users from the db
+//make sure to update in real time, run query every 5 seconds or something

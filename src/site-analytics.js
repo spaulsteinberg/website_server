36bbeb8eff034fdde4ad0f03fd02b082cf1d66c8
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-08-26 22:00:43 -0500

    added line

--- src/site-analytics.js
+++ src/site-analytics.js
@@ -3,3 +3,4 @@
 //google analytic api -- use map locations and overall data
 // grab the new users from the db
 //make sure to update in real time, run query every 5 seconds or something
+//will probably need ajax call or something
